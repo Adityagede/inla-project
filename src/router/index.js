bc75e3f09ac5bc80b35d406c0b2d53c084737668
Gede-aditya <gedeaditya110@gmail.com>
2026-07-05 20:20:06 +0800

    cobacoba

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Beranda from '../pages/Beranda.vue'
 import TentangKami from '../pages/TentangKami.vue'
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(/inla-project/),
+  history: createWebHashHistory(), // 🔥 INI YANG BENAR
   routes,
   scrollBehavior() {
     return { top: 0 }
