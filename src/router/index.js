17f560bbf092cb98e0b84afa2c0c5d20d09423e3
Gede-aditya <gedeaditya110@gmail.com>
2026-07-05 20:07:05 +0800

    cobalagi

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(/inla-project/),
   routes,
   scrollBehavior() {
     return { top: 0 }
